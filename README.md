90d8964f02299ddb4a485dcff90f47ba701fa40c
Christopher Kullenberg <christopher.kullenberg@gmail.com>
2017-11-20 21:37:47 +0100

    fixed readme and more output files

---
+++ README.md
@@ -0,0 +1,4 @@
+#CV Christopher Kullenberg
+
+My CV in various file formats. Generated from markdown with pandoc. See
+``compile.sh`` for settings.
--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 #pandoc -V fontsize=12pt -V papersize:"a4paper" -V geometry:"top=3cm, bottom=3cm, left=3cm, right=3cm" cvkullenberg.md -o cvkullenberg.pdf
 pandoc -V fontsize=12pt -V papersize:"a4paper" -V geometry:"top=3cm, bottom=3cm, left=3cm, right=3cm" --toc --toc-depth=2 cvkullenberg.md -o cvkullenberg.pdf
+pandoc -V fontsize=12pt -V papersize:"a4paper" -V geometry:"top=3cm, bottom=3cm, left=3cm, right=3cm" --toc --toc-depth=2 cvkullenberg.md -o cvkullenberg.docx
+pandoc -V fontsize=12pt -V papersize:"a4paper" -V geometry:"top=3cm, bottom=3cm, left=3cm, right=3cm" --toc --toc-depth=2 cvkullenberg.md -o cvkullenberg.odt
+#pandoc  --toc --toc-depth=2 cvkullenberg.md -o cvkullenberg.html
